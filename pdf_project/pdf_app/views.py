c9e61b51bd4a3b89c5f892748b5597b14a97145f
OwerFrank <olopeza@unsa.edu.pe>
2023-07-14 00:06:29 -0500

    Agregamos loso CSS para neustro formulario

--- pdf_project/pdf_app/views.py
+++ pdf_project/pdf_app/views.py
@@ -8,23 +8,18 @@ from xhtml2pdf import pisa
 
 class PDFView(View):
     def get(self, request):
-        # Renderizar el formulario para que el usuario ingrese los datos
         return render(request, 'pdf_app/formulario.html')
 
     def post(self, request):
-        # Obtener los datos ingresados por el usuario
         nombre = request.POST.get('nombre', '')
         contenido = request.POST.get('contenido', '')
 
-        # Obtener el contenido HTML de la plantilla
         template = get_template('pdf_app/pdf_template.html')
         context = {'nombre': nombre, 'contenido': contenido}
         html = template.render(context)
 
-        # Crear el archivo PDF
         pdf_file = self.create_pdf(html)
 
-        # Enviar el PDF como respuesta
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'inline; filename="ArchivoDescargadoYCreadoPorOwer.pdf"'
         response.write(pdf_file.getvalue())
@@ -32,10 +27,8 @@ class PDFView(View):
         return response
 
     def create_pdf(self, html):
-        # Crear un archivo PDF en memoria
         pdf_file = BytesIO()
 
-        # Generar el PDF a partir del contenido HTML
         pisa.CreatePDF(html, dest=pdf_file)
 
         return pdf_file
@@ -43,23 +36,18 @@ class PDFView(View):
 
 class PDFDownloadView(View):
     def get(self, request):
-        # Renderizar el formulario para que el usuario ingrese los datos
         return render(request, 'pdf_app/formulario.html')
 
     def post(self, request):
-        # Obtener los datos ingresados por el usuario
         nombre = request.POST.get('nombre', '')
         contenido = request.POST.get('contenido', '')
 
-        # Obtener el contenido HTML de la plantilla
         template = get_template('pdf_app/pdf_template.html')
         context = {'nombre': nombre, 'contenido': contenido}
         html = template.render(context)
 
-        # Crear el archivo PDF
         pdf_file = PDFView().create_pdf(html)
 
-        # Enviar el PDF como descarga
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="ArchivoDescargadoYCreadoPorOwer.pdf"'
         response.write(pdf_file.getvalue())
