4aaf867f739d23de958ad47a68682adf08f0b117
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 22:24:21 -0500

    modificamos el views.py por el error que me mandaba

--- pdf_project/pdf_app/views.py
+++ pdf_project/pdf_app/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
 from django.views import View
-from easy_pdf.rendering import render_to_pdf_response
+from reportlab.pdfgen import canvas
 
 
 class PDFView(View):
     def get(self, request):
         data = {
-            'nombre': 'Ower',
-            'apellido': 'Lopez',
-            'edad': 18
+            'nombre': 'Juan',
+            'apellido': 'Pérez',
+            'edad': 25
         }
-        pdf = render_to_pdf_response('pdf_app/template.html', data)
-        return HttpResponse(pdf, content_type='application/pdf')
-    
+
+        template = get_template('pdf_app/template.html')
+        html = template.render(data)
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'filename="archivo.pdf"'
+
+        p = canvas.Canvas(response)
+
+        p.setFont("Helvetica", 12)
+        p.drawString(100, 700, "Información Personal")
+        p.drawString(100, 650, "Nombre: {}".format(data['nombre']))
+        p.drawString(100, 600, "Apellido: {}".format(data['apellido']))
+        p.drawString(100, 550, "Edad: {}".format(data['edad']))
+
+        p.showPage()
+        p.save()
+
+        return response
--- pdf_project/pdf_project/urls.py
+++ pdf_project/pdf_project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pdf_app/', include('pdf_app.urls', namespace='pdf_app')),
 ]
