9d45874484104f1140160809f31a876691fed518
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 22:09:03 -0500

    Creo el views.py

--- pdf_project/pdf_app/views.py
+++ pdf_project/pdf_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from easy_pdf.rendering import render_to_pdf_response
 
-# Create your views here.
+
+class PDFView(View):
+    def get(self, request):
+        data = {
+            'nombre': 'Ower',
+            'apellido': 'Lopez',
+            'edad': 18
+        }
+        pdf = render_to_pdf_response('pdf_app/template.html', data)
+        return HttpResponse(pdf, content_type='application/pdf')
