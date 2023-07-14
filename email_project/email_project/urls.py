5e655307584849c60318e4120920e54ba5d7c619
OwerFrank <olopeza@unsa.edu.pe>
2023-07-14 00:22:17 -0500

    Añado los urls que serviran en el rpoyecto

--- email_project/email_project/urls.py
+++ email_project/email_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from email_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('send_email/', views.send_email, name='send_email'),
+    path('success/', views.success, name='success'),
 ]
