0faef0c4a4b44ea919b0235deedb941076893144
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 20:31:26 -0500

    defino los modelos para la relación de uno a muchos

--- rumo_project/rumo_app/models.py
+++ rumo_project/rumo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
