96fe9be1580727f206bc86aff86120e8ee000e88
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 23:33:39 -0500

    Mejoramos el views.py de nuestro proyecto

--- pdf_project/pdf_app/urls.py
+++ pdf_project/pdf_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PDFView
+from .views import PDFView, PDFDownloadView
 
 app_name = 'pdf_app'
 
 urlpatterns = [
     path('pdf/', PDFView.as_view(), name='pdf'),
+    path('download/', PDFDownloadView.as_view(), name='download'),
 ]
--- pdf_project/pdf_app/views.py
+++ pdf_project/pdf_app/views.py
@@ -1,21 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
-from django.views.generic import View
+from django.views import View
 from io import BytesIO
 from xhtml2pdf import pisa
 
+
 class PDFView(View):
     def get(self, request):
-        template = get_template('pdf_app/pdf_template.html')
-        context = {'nombre_variable': 'Hola, mundo!'}
-        html = template.render(context)
-        response = BytesIO()
-        pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
-        
-        if not pdf.err:
-            response = HttpResponse(response.getvalue(), content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment; filename="archivo.pdf"'
-            return response
+        # Renderizar el formulario para que el usuario ingrese los datos
+        return render(request, 'pdf_app/formulario.html')
 
-        return HttpResponse("Error al generar el PDF", status=500)
+    def post(self, request):
+        # Obtener los datos ingresados por el usuario
+        nombre = request.POST.get('nombre', '')
+        contenido = request.POST.get('contenido', '')
+
+        # Obtener el contenido HTML de la plantilla
+        template = get_template('pdf_app/pdf_template.html')
+        context = {'nombre': nombre, 'contenido': contenido}
+        html = template.render(context)
+
+        # Crear el archivo PDF
+        pdf_file = self.create_pdf(html)
+
+        # Enviar el PDF como respuesta
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'inline; filename="ArchivoDescargadoYCreadoPorOwer.pdf"'
+        response.write(pdf_file.getvalue())
+
+        return response
+
+    def create_pdf(self, html):
+        # Crear un archivo PDF en memoria
+        pdf_file = BytesIO()
+
+        # Generar el PDF a partir del contenido HTML
+        pisa.CreatePDF(html, dest=pdf_file)
+
+        return pdf_file
+
+
+class PDFDownloadView(View):
+    def get(self, request):
+        # Renderizar el formulario para que el usuario ingrese los datos
+        return render(request, 'pdf_app/formulario.html')
+
+    def post(self, request):
+        # Obtener los datos ingresados por el usuario
+        nombre = request.POST.get('nombre', '')
+        contenido = request.POST.get('contenido', '')
+
+        # Obtener el contenido HTML de la plantilla
+        template = get_template('pdf_app/pdf_template.html')
+        context = {'nombre': nombre, 'contenido': contenido}
+        html = template.render(context)
+
+        # Crear el archivo PDF
+        pdf_file = PDFView().create_pdf(html)
+
+        # Enviar el PDF como descarga
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="ArchivoDescargadoYCreadoPorOwer.pdf"'
+        response.write(pdf_file.getvalue())
+
+        return response
