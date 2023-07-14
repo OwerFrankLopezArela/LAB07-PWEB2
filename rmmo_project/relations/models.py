f15f7b9fc0d4e2623b478f86500e3fd1fde13d6b
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 21:47:28 -0500

    Creamos nuestro modelo apra apreciar la relacion

--- rmmo_project/relations/models.py
+++ rmmo_project/relations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    participants = models.ManyToManyField(Person)
+
+    def __str__(self):
+        return self.name
