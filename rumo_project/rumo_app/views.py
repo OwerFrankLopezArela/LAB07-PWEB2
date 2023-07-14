e4b64ca0adb2552ac8c70011bf44e8b008d5ef0f
OwerFrank <olopeza@unsa.edu.pe>
2023-07-13 20:36:59 -0500

    defino las vistas para mostrar los detalles del proyecto y las tareas asociadas

--- rumo_project/rumo_app/views.py
+++ rumo_project/rumo_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Proyecto
 
-# Create your views here.
+def detalle_proyecto(request, proyecto_id):
+    proyecto = get_object_or_404(Proyecto, pk=proyecto_id)
+    return render(request, 'rumo_app/detalle_proyecto.html', {'proyecto': proyecto})
