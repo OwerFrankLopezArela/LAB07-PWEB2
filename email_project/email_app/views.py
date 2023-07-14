87cbafe181ce25b4e03a7b467dc927bef0f4ab4e
OwerFrank <olopeza@unsa.edu.pe>
2023-07-14 00:20:31 -0500

    Creacion de mi vista del gmail

--- email_project/email_app/views.py
+++ email_project/email_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .forms import EmailForm
+from .models import Email
 
-# Create your views here.
+def send_email(request):
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.save()
+            send_mail(
+                email.subject,
+                email.message,
+                'your_email@example.com',  # Reemplaza con tu dirección de email
+                [email.recipient],
+                fail_silently=False,
+            )
+            email.sent = True
+            email.save()
+            return redirect('success')
+    else:
+        form = EmailForm()
+    return render(request, 'email_app/email.html', {'form': form})
+
+def success(request):
+    return render(request, 'email_app/success.html')
